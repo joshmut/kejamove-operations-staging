83172f0fc537648e07af88438d703914acc5624b
Joshua Mutua <joshuamutua39@gmail.com>
2026-08-13 06:32:43 +0300

    deploy completed operations first release

---
+++ assets/subDays-LeXcRvRu.js
@@ -0,0 +1,1 @@
+import{az as r,aA as e}from"./index-CACZmIMz.js";function n(s,t){const a=r(s);return isNaN(t)?e(s,NaN):(t&&a.setDate(a.getDate()+t),a)}function u(s){const t=r(s);return t.setDate(1),t.setHours(0,0,0,0),t}function c(s,t){return n(s,-t)}export{n as a,c as b,u as s};
